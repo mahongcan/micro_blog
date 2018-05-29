222ce23d99a80054913ff9c0326871034e867660
mahongcan <make835215945@163.com>
2018-05-29 09:22:14 +0800

    修改标题

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -38,7 +38,7 @@
 <body>
 <div class="container">
     <div class="content">
-        <div class="title">Hello World 马可</div>
+        <div class="title">Hello World 马可123</div>
     </div>
 </div>
 </body>
